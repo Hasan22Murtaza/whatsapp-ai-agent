4cbe2ce9e11fa7aa80298ed2c48be5bbfc7c3dd7
Hasan Murtaza <hasanmurtaza88@gmail.com>
2025-02-05 11:47:36 +0500

    'init'

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const messageRouter = require('./routers/messageRouter')
+const whatsappclient = require('./services/WhatsappClient')
+
+whatsappclient.initialize();
+const app = express()
+app.use(express.json())
+app.use(messageRouter)
+
+app.listen(4000, () => console.log(`Server is ready in on port ${4000}`))
